c669b527c01da5cdd94f2d3848480fc300fd1462
tusharbhambere <tusharbhambere8@gmail.com>
2021-08-07 14:13:23 +0530

    final

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import { routes } from "./routes/wall.routes";
 dotenv.config();
 const app=express()
 
-const port=8080;
+
+
+
+const port=process.env.PORT || 8080;
 app.set("port",port)
 app.use(cors())
 app.use(express.json())
